35702c7f7bf9e30d6a176fef717cc1e0e5583e33
shoxjahon975 <126753362+shoxjahon975@users.noreply.github.com>
2023-03-04 11:20:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ lastnmae=input("Familya kriting")
 age=input("Yoshingizni kriting")
 email=input("Emailingzni kriting")
 password=input("Parolingizni kriting")
+print=("firstname,lastname,age,email,password)
+
+a=5
+b=8
+c=9+5*8
+print(c)
+       
+a=14
+b=21
+c=15+14*21
+print(C)       
