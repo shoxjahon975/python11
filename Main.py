8cb0989027266aa74ab60a405e8ea6ce1cef0069
shoxjahon975 <126753362+shoxjahon975@users.noreply.github.com>
2023-03-04 20:53:46 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+login=input('Loginni kriting:')
+parol=input('Parolni kriting:')
+email=input('Emailni kriting:')
+if login == 'Shoxjahon':
+    print('Xush kelebsiz Shoxjahon')
+elif login == 'admin':
+    print('Men sizni tanimadim')
+if parol == '123456':
+    print('')
+elif parol == '654321':
+    print('Siz parolni xato kritingiz')
+if email == 'shoxjaxonkarimov':
+    print ==('Siz emailni tugri krittiz')
+elif email == 'adminkarimov':
+    print ==('Emailni tugri krittiz')
