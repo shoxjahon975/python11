ea8579ffcc1c0fb8b699f745cf5be50709f6b9ed
shoxjahon975 <126753362+shoxjahon975@users.noreply.github.com>
2023-03-04 11:33:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ email=input("Emailingzni kriting")
 password=input("Parolingizni kriting")
 print=("firstname,lastname,age,email,password)
 
+# a va b sonlarni c formula bilan ishlang.
 a=5
 b=8
 c=9+5*8
 print(c)
-       
+
+# a va b sonlarini ayiring.
 a=14
 b=21
-c=15+14*21
+c=15-14
 print(C)       
