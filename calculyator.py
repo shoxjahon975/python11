19411bb8ab937e4408a91957ec6c4a5984d025ee
shoxjahon975 <126753362+shoxjahon975@users.noreply.github.com>
2023-03-05 09:56:01 +0300

    Add files via upload

---
+++ calculyator.py
@@ -0,0 +1,13 @@
+a=int(input('Birinchi soni kriting: '))
+b=int(input('Ikinchi soni kriting: '))
+c=input('Amalni kriting:')
+if c== '+':
+    print('Natija: ', a+b)
+elif c== '-' :
+      print('Natija: ', a-b)  
+elif c== '*' :
+      print('Natija: ', a*b)
+elif c== '/' :
+      print('Natija: ', a/b)   
+elif c== '**' :
+      print('Natija: ', a**b)               
